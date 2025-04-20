4f178a111fe1bcdb3f911de639ef81193d4f2c4a
davidmaniibrahim <ibrahimmanidavid@gmail.com>
2025-04-20 20:55:39 +0100

    fix: fix bug on project component

--- src/Components/Experience/Experience.jsx
+++ src/Components/Experience/Experience.jsx
@@ -1,4 +1,7 @@
 import styles from "./Experience.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle, faMicrosoft } from '@fortawesome/free-brands-svg-icons';
+
 
 const Experience = () => {
   return (
@@ -11,7 +14,8 @@ const Experience = () => {
       <div className={styles.experienceProjects}>
         <div className={styles.experience}>
           <div className={styles.google}>
-            <img src="" alt="google logo" />
+            {/* <img src="" alt="google logo" /> */}
+            <FontAwesomeIcon icon={faGoogle} size="4x" />
             <div className={styles.googleContent}>
               <h3>Software Engineer, Google</h3>
               <span>Sept, 2022 - Present</span>
@@ -22,25 +26,32 @@ const Experience = () => {
             </div>
           </div>
 
+       
+
+          <div className={styles.netflix}>
+            
+            <img src="/netflix.png" alt="Netflix Logo" width={80} />
+
+
+            <div className={styles.netflixContent}>
+              <h3>SWE Intern, Netflix</h3>
+              <span>April, 2020 - Jan, 2021</span>
+              <ul>
+                <li>Worked on component library</li>
+              </ul>
+            </div>
+
+          </div>
+
           <div className={styles.microsoft}>
-            <img src="" alt="microsoft logo" />
+            {/* <img src="" alt="microsoft logo" /> */}
+            <FontAwesomeIcon icon={faMicrosoft} size="4x" />
             <div className={styles.microsoftContent}>
               <h3>UI Designer, Microsoft</h3>
               <span>Aug, 2021 - Aug, 2022</span>
               <ul>
                 <li>Worked on Windows 11</li>
                 <li>Designed the control panel</li>
-              </ul>
-            </div>
-          </div>
-
-          <div className={styles.netflix}>
-            <img src="" alt="netflix logo" />
-            <div className={styles.netflixContent}>
-              <h3>SWE Intern, Netflix</h3>
-              <span>April, 2020 - Jan, 2021</span>
-              <ul>
-                <li>Worked on component library</li>
               </ul>
             </div>
           </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,29 +1,18 @@
-import styles from "./Projects.module.css";
+import Items from "./Items"
 
 const Projects = () => {
+  const project1 = {title: 'Voting App', description: 'Made a Voting App with react and django'}
+  const project2 = {title: 'CRMS', description: 'Made a customer relationship management system'}
+  const project3 = {title: 'Ecommerce', description: 'Made an ecommerce App with Rect and Django'}
   return (
-    <div className={styles.projectSection}>
-      {["A", "B", "C"].map((project, index) => (
-        <div className={styles.projectContainer} key={index}>
-          <div className={styles.projectContent}>
-            <h2>Project {project}</h2>
-            <p>
-              This is a project made to learn the latest languages by building an app
-            </p>
-            <div className={styles.lang}>
-              <button>React</button>
-              <button>Express</button>
-              <button>Node</button>
-            </div>
-            <div className={styles.btn}>
-              <button>Demo</button>
-              <button>Source</button>
-            </div>
-          </div>
-        </div>
-      ))}
-    </div>
+    <>
+    <Items title="" description={project1}/>
+    <Items title="" description={project2}/>
+    <Items title="" description={project3}/>
+    </>
   );
 };
 
 export default Projects;
+
+
